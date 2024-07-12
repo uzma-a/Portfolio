9e1f3dc8c3b6ee615a5ddf8cef188d855954963c
uzma-a <uzmaaasia444@gmail.com>
2024-07-12 07:47:53 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'custom': {'max': '710px'},
+      }
+    },
+  },
+  plugins: [],
+}
